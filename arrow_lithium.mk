eeddbbf270bf486019d98513ddea17e11919a97e
rik0612c <qq872638399@gmail.com>
2023-02-23 23:33:13 +0800

    lithium: remove fingerprint overrides

--- arrow_lithium.mk
+++ arrow_lithium.mk
@@ -29,10 +29,3 @@ PRODUCT_MODEL := MI MIX
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="lithium" \
-    PRIVATE_BUILD_DESC="lithium-user 8.0.0 OPR1.170623.032 V9.6.1.0.OAHMIFD release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/lithium/lithium:8.0.0/OPR1.170623.032/V9.6.1.0.OAHMIFD:user/release-keys"
